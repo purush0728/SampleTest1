b09bb477cce066cdb53ee9aafb26cf71bd9f27e8
laxman <laxman@rizecorp.com>
2015-10-15 13:05:56 +0530

    updated sample project

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by ramakrishna on 10/15/15.
+//  Copyright (c) 2015 rize. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
